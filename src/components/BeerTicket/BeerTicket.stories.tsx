29186c53d6f9b811d704692e557aa182f4348dd8
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-06-05 09:22:19 +0900

    feat(ui): implement BeerTicket.stories module

---
+++ src/components/BeerTicket/BeerTicket.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Record } from '@/constants/Record';
+
+import BeerTicket from './BeerTicket';
+
+export default {
+  title: 'Components/BeerTicket',
+  component: BeerTicket,
+} as ComponentMeta<typeof BeerTicket>;
+
+const Template: ComponentStory<typeof BeerTicket> = (args) => <BeerTicket {...args} />;
+
+export const 티켓 = Template.bind({});
+티켓.args = {
+  record: Record,
+};
+
+export const 티켓이랑도장 = Template.bind({});
+티켓이랑도장.args = {
+  record: Record,
+  type: 'stamp',
+};
+
+export const 티켓이랑도장2 = Template.bind({});
+티켓이랑도장2.args = {
+  record: { ...Record, feel: 5, recodedAt: new Date(2022, 1, 1) } as any,
+  type: 'stamp',
+};
