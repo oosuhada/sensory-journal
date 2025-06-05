eaa71b23e6f198cc407bf1721156f4ee805a1a6d
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-06-05 22:24:33 +0900

    feat(ui): implement Emoji module

---
+++ src/components/Emoji/Emoji.tsx
@@ -0,0 +1,30 @@
+import Icon from '@/components/commons/Icon';
+
+const getEmoji = (feel?: number) => {
+  switch (feel) {
+    case 1:
+      return 'Chaos';
+    case 2:
+      return 'Bad';
+    case 3:
+      return 'Soso';
+    case 4:
+      return 'Good';
+    default:
+      return 'Best';
+  }
+};
+
+interface EmojiProps {
+  feel?: number | null;
+  className?: string;
+  size?: number;
+}
+
+const Emoji = (props: EmojiProps) => {
+  const { feel, className, size = 36 } = props;
+
+  return <>{feel !== null && <Icon name={getEmoji(feel)} size={size} className={className} />}</>;
+};
+
+export default Emoji;
