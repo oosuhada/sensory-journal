3aba081ef87fe252c7ea5ecf2ca6121aa8f98449
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-06-14 10:00:36 +0900

    test: add component development and delivery tooling

---
+++ .storybook/main.js
@@ -0,0 +1,65 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    'storybook-dark-mode',
+    'storybook-addon-next-router',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, '../src')],
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 120, singleQuote: true },
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+
+    /**
+     * 동일한 이슈가 있어 comment에 있는 solution대로 적용
+     * https://github.com/storybookjs/storybook/issues/16690
+     */
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    /** refs: https://minoo.medium.com/storybook%EC%97%90%EC%84%9C-nextjs-%EC%97%90%EB%9F%AC%EA%B0%80-%EB%B0%9C%EC%83%9D%ED%95%A0-%EB%95%8C-85ec0b3662af */
+    config.resolve.alias['next/router'] = require.resolve('../__mocks__/next/router.js');
+    config.resolve.alias['next/link'] = require.resolve('../__mocks__/next/link.js');
+    config.resolve.alias['next/image'] = require.resolve('../__mocks__/next/image.js');
+
+    return config;
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,62 @@
+import { Global, css } from '@emotion/react';
+import { QueryClientProvider } from 'react-query';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { ThemeProvider } from 'emotion-theming';
+import { RecoilRoot } from 'recoil';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+import queryClient from '../src/configs/queryClient';
+import MainLayout from '../src/components/layouts/MainLayout';
+import { theme, GlobalStyle } from '../src/themes';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphone12',
+  },
+  layout: 'fullscreen',
+  options: {
+    storySort: {
+      order: ['Design System', 'Commons', 'Components', 'Pages'],
+    },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle theme={theme} />
+          <Global
+            styles={css`
+              html,
+              #root {
+                height: 100%;
+              }
+
+              body {
+                height: 100%;
+                padding: 0 !important;
+                background-color: ${({ theme }) => theme.semanticColor.background};
+              }
+            `}
+          />
+          <MainLayout>
+            <Story />
+          </MainLayout>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  ),
+];
