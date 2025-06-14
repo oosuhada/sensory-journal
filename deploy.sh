5efb07e2f8acd66061eabc573b2708321fef5592
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-06-14 11:11:53 +0900

    docs: document sensory journal deployment

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+cd ~/sulsul-FE
+
+git checkout master
+
+git pull
+
+yarn install
+yarn build
+
+# pm2 ecosystem deploy
+if pm2 list | grep -q sulsul-fe
+then
+  pm2 reload pm2.yml --only sulsul-fe
+  echo "pm2 reload"
+else
+  pm2 start pm2.yml --only sulsul-fe
+  echo "pm2 start"
+fi
