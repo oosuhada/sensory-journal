47b5548b08f79dc29863a5b1fb28e463bc462bbf
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-10-01 19:27:22 +0900

    feat(ui): implement Top3BeerFlavorList module

---
+++ src/constants/Top3BeerFlavorList.ts
@@ -0,0 +1,16 @@
+import { ITop3BeerFlavor } from '@/apis';
+
+export const Top3BeerFlavorList: ITop3BeerFlavor[] = [
+  {
+    content: '목넘김이 부드러워요',
+    count: 30,
+  },
+  {
+    content: '달달해요',
+    count: 24,
+  },
+  {
+    content: '과일 향이 나요',
+    count: 47,
+  },
+];
